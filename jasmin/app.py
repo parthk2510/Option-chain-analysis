eb69e6d30eb549c9675fdda5c1e101a4e49c0695
jasmindhedhi <72064257+jasmindhedhi@users.noreply.github.com>
2023-07-04 20:45:28 +0530

    jd3

--- jasmin/app.py
+++ jasmin/app.py
@@ -18,12 +18,20 @@ def get_data():
         document['_id'] = str(document['_id'])  # Convert ObjectId to string
         data.append(document)
         
-        if i == 100:
+        if i == 1000:
             break
     
     symbol = request.args.get("symbol")
-    if symbol:
+    expiry_date = request.args.get("expiry_date")
+    
+    if symbol and expiry_date:
+        filtered_data = [item for item in data if item["symbol"] == symbol and item["expiry_date"] == expiry_date]
+        return jsonify(filtered_data)
+    elif symbol:
         filtered_data = [item for item in data if item["symbol"] == symbol]
+        return jsonify(filtered_data)
+    elif expiry_date:
+        filtered_data = [item for item in data if item["expiry_date"] == expiry_date]
         return jsonify(filtered_data)
     else:
         return jsonify(data)
@@ -32,6 +40,17 @@ def get_data():
 def get_symbols():
     symbols = collection.distinct("symbol")
     return jsonify(symbols)
+
+@app.route('/expiry-dates', methods=['GET'])
+def get_expiry_dates():
+    symbol = request.args.get("symbol")
+    
+    if symbol:
+        expiry_dates = collection.distinct("expiry_date", {"symbol": symbol})
+    else:
+        expiry_dates = collection.distinct("expiry_date")
+    return jsonify(expiry_dates)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
